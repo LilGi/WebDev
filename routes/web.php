cb8bf0fcb040dc547f11b83ff95b4f64e461a12b
LilGi <91885108+LilGi@users.noreply.github.com>
2021-12-10 19:42:40 +0800

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+
+Route::get('qr', function () {
+    return view('layouts.qr');
+})->middleware(['auth'])->name('qr');
+
+Route::get('account-settings', function () {
+    return view('layouts.account-settings');
+})->middleware(['auth'])->name('account-settings');
+
+Route::get('home', function () {
+    return view('layouts.home');
+})->middleware(['auth'])->name('home');
+
+Route::get('own-logs', function () {
+    return view('layouts.own-logs');
+})->middleware(['auth'])->name('own-logs');
+
+Route::get('main', function () {
+    return view('layouts.main');
+})->middleware(['auth'])->name('main');
+
+
+
+
+require __DIR__.'/auth.php';
